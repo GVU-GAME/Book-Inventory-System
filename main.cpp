8b64fc2962e8bf71bfe6eda8aa03ca53b1322e38
GVU-GAME <56762234+GVU-GAME@users.noreply.github.com>
2021-07-20 21:53:13 -0400

    set up functionalities of app

--- main.cpp
+++ main.cpp
@@ -4,6 +4,34 @@ int main() {
     try
     {
         /* code */
+        book inventory[20];
+        int choice;
+        while(true) {
+            cout << "1) Search for a book" << endl;
+            cout << "2) Buy a book" << endl;
+            cout << "3) Enter a new book" << endl;
+            cout << "4) Edit a book info" << endl;
+            cout << "Choice: ";
+            cin >> choice;
+
+            switch (choice)
+            {
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+            
+            default:
+                break;
+            }
+        }
     }
     catch(const std::exception& e)
     {
